294835d580cbd4b172884cd7a87d7663ab4fc61b
Jonathan Cruz <j.cruz@utp.edu.co>
2023-04-18 17:38:39 -0500

    first commit

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -24,8 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
+COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
+
 
 ALLOWED_HOSTS = []
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
