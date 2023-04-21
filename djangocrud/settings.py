2089a7a2de0ab62638bb41c9006f6f94dda309e6
Jonathan Cruz <j.cruz@utp.edu.co>
2023-04-21 09:11:34 -0500

    corsheaders

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -47,11 +47,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tasks.apps.TasksConfig',
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -80,6 +82,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'djangocrud.wsgi.application'
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 
 # Database
